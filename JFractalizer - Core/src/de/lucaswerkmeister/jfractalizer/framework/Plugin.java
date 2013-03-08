1c05a93ceb37d1b49d0566bf789f476dc766adac
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-03-08 11:53:42 +0100

    Added vendor-, plugin name and logToString to Plugins

--- JFractalizer - Core/src/de/lucaswerkmeister/jfractalizer/framework/Plugin.java
+++ JFractalizer - Core/src/de/lucaswerkmeister/jfractalizer/framework/Plugin.java
@@ -11,4 +11,29 @@ public interface Plugin {
 	 * Registers log IDs.
 	 */
 	public void registerLogIDs();
+
+	/**
+	 * Gets the plugin vendor's name.
+	 * 
+	 * @return The vendor name.
+	 */
+	public String getVendorName();
+
+	/**
+	 * Gets the plugin's name.
+	 * 
+	 * @return The plugin name.
+	 */
+	public String getPluginName();
+
+	/**
+	 * Decodes a logging entry into a string.
+	 * 
+	 * @param id
+	 *            The log ID.
+	 * @param args
+	 *            The arguments of the logging entry.
+	 * @return A string representing the logging entry.
+	 */
+	public String logToString(int id, Object... args);
 }
