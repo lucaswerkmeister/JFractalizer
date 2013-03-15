c2abc7f8c99d2e7020b2474678749e4640ce2e40
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-03-15 10:28:58 +0100

    Implemented the TimeSpan.

--- JFractalizer - Core/src/de/lucaswerkmeister/jfractalizer/core/TimeSpan.java
+++ JFractalizer - Core/src/de/lucaswerkmeister/jfractalizer/core/TimeSpan.java
@@ -9,6 +9,8 @@ import java.util.Locale;
  * @version 1.0
  */
 public class TimeSpan {
+	private final long	nanos;
+
 	/**
 	 * Creates a new {@link TimeSpan} with the specified duration.
 	 * 
@@ -16,7 +18,7 @@ public class TimeSpan {
 	 *            The duration in nanoseconds.
 	 */
 	public TimeSpan(long nanos) {
-
+		this.nanos = nanos;
 	}
 
 	/**
@@ -30,5 +32,62 @@ public class TimeSpan {
 	 */
 	public TimeSpan(long nanos, Locale locale) {
 		this(nanos);
+	}
+
+	public String toString() {
+		long interval = nanos / 1_000_000;
+		final short milliseconds = (short) (interval % 1000);
+		interval -= milliseconds;
+		interval /= 1000;
+		final byte seconds = (byte) (interval % 60);
+		interval -= seconds;
+		interval /= 60;
+		final byte minutes = (byte) (interval % 60);
+		interval -= minutes;
+		interval /= 60;
+		final byte hours = (byte) (interval % 24);
+		interval -= hours;
+		interval /= 24;
+		final long days = interval;
+		final StringBuilder ret = new StringBuilder();
+		if (days != 0) {
+			ret.append(days);
+			ret.append(" day");
+			if (days != 1)
+				ret.append('s');
+			ret.append(' ');
+		}
+		if (hours != 0) {
+			ret.append(hours);
+			ret.append(" hour");
+			if (hours != 1)
+				ret.append('s');
+			ret.append(' ');
+		}
+		if (minutes != 0) {
+			ret.append(minutes);
+			ret.append(" minute");
+			if (minutes != 1)
+				ret.append('s');
+			ret.append(' ');
+		}
+		if (seconds != 0) {
+			ret.append(seconds);
+			ret.append(" second");
+			if (seconds != 1)
+				ret.append('s');
+			ret.append(' ');
+		}
+		if (milliseconds != 0) {
+			ret.append(milliseconds);
+			ret.append(" millisecond");
+			if (milliseconds != 1)
+				ret.append('s');
+			ret.append(' ');
+		}
+		ret.append('(');
+		ret.append(nanos);
+		ret.append(" ns).");
+		return ret.toString();
 	}
 }
